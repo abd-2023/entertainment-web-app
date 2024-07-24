1dabe17e7cea072a98045fa13c289362dbace9c5
abd-2023 <abcoding17@gmail.com>
2024-07-24 09:10:15 +0530

    feat: Add NavBar, install MUI Icons

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { AppBar, Container, Toolbar } from "@mui/material"
+import { AccountCircle, Bookmark, LiveTv, Movie, Theaters, Window } from "@mui/icons-material"
+
+const NavBar = () => {
+  return (
+    <AppBar>
+      <Container>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Movie />
+          <Toolbar sx={{ gap: 2 }} >
+            <Window />
+            <Theaters />
+            <LiveTv />
+            <Bookmark />
+          </Toolbar>
+          <AccountCircle />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
+
+export default NavBar
