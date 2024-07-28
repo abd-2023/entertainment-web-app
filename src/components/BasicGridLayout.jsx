ff2607c16892364c93fb59e9bec57de4247d4981
abd-2023 <abcoding17@gmail.com>
2024-07-28 09:44:39 +0530

    feat: Add Header, List for grid items

--- src/components/BasicGridLayout.jsx
+++ src/components/BasicGridLayout.jsx
@@ -1,13 +1,44 @@
-import { Grid } from '@mui/material'
-import SimpleCard from './components/SimpleCard'
+import { Box, Grid, Stack, Typography } from '@mui/material'
+import SimpleCard from './SimpleCard'
+import { v4 as uuidv4 } from 'uuid'
 
-const BasicGridLayout = () => {
+const BasicGridLayout = ({
+	Heading,
+	list,
+	titleProp,
+	dateProp,
+	imageURLProp,
+}) => {
+	function getFullYear(formattedDate) {
+		const date = new Date(formattedDate)
+		const fullYear = date.getFullYear()
+		return fullYear
+	}
+
 	return (
-		<Grid container spacing={2} sx={{ px: 2 }}>
-			<Grid item xs={6}>
-				<SimpleCard />
+		<Box>
+			<Stack
+				direction="row"
+				spacing={2}
+				justifyContent="space-between"
+				alignItems="center"
+			>
+				<Typography variant="h6"> {Heading} </Typography>
+				<Typography>see more</Typography>
+			</Stack>
+			<Grid container spacing={2}>
+				{list.map((item) => (
+					<Grid item xs={6} key={uuidv4()}>
+						<SimpleCard
+							mediaTitle={item[titleProp]}
+							year={getFullYear(item[dateProp])}
+							mediaType="movie"
+							imageURL={item[imageURLProp]}
+						/>
+					</Grid>
+				))}
 			</Grid>
-		</Grid>
+		</Box>
 	)
 }
 
