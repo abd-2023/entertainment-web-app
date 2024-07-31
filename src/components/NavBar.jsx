c3db0ea300c99746bc30f5d1464c703790181b59
abd-2023 <abcoding17@gmail.com>
2024-07-31 17:39:32 +0530

    refactor: Add List in NavBar
    - use List component for semantics & accessibility

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,37 @@
-import { AppBar, Avatar, Container, Toolbar } from '@mui/material'
+import { AppBar, Avatar, List, ListItem } from '@mui/material'
 import { Bookmark, LiveTv, Movie, Theaters, Window } from '@mui/icons-material'
 
 const NavBar = () => {
 	return (
 		<AppBar sx={{ position: 'sticky' }}>
-			<Container>
-				<Toolbar sx={{ justifyContent: 'space-between' }}>
+			<List sx={{ display: 'flex', height: '3.5rem' }}>
+				<ListItem sx={{ p: 0, mr: 5, justifyContent: 'end' }}>
 					<Movie />
-					<Toolbar sx={{ gap: 2 }}>
-						<Window />
-						<Theaters />
-						<LiveTv />
-						<Bookmark />
-					</Toolbar>
+				</ListItem>
+				<ListItem sx={{ p: 0, justifyContent: 'center' }}>
+					<Window />
+				</ListItem>
+				<ListItem sx={{ p: 0, justifyContent: 'center' }}>
+					<Theaters />
+				</ListItem>
+				<ListItem sx={{ p: 0, justifyContent: 'center' }}>
+					<LiveTv />
+				</ListItem>
+				<ListItem sx={{ p: 0, justifyContent: 'center' }}>
+					<Bookmark />
+				</ListItem>
+				<ListItem sx={{ p: 0, ml: 5, justifyContent: 'start' }}>
 					<Avatar
 						alt="Remy Sharp"
 						src="https://mui.com/static/images/avatar/1.jpg"
 						sx={{
 							border: 3,
+							width: '1.5em',
+							height: '1.5em',
 						}}
 					/>
-				</Toolbar>
-			</Container>
+				</ListItem>
+			</List>
 		</AppBar>
 	)
 }
