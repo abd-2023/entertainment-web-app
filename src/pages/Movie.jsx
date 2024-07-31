7bb365c79046170d15260eafb3d0ecc891706ba0
abd-2023 <abcoding17@gmail.com>
2024-07-31 16:28:15 +0530

    feat: Add GenreGridLayout to Movie

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,5 +1,12 @@
+import { Box } from '@mui/material'
+import GenreGridLayout from '../components/GenreGridLayout'
+
 const Movie = () => {
-	return <div>Movie</div>
+	return (
+		<Box>
+			<GenreGridLayout genres={genres} />
+		</Box>
+	)
 }
 
 export default Movie
