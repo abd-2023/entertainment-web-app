55a2c4baf496665bb17235559e691f4aaec59d73
abd-2023 <abcoding17@gmail.com>
2024-08-15 17:45:32 +0530

    feat: Add Outfit google font

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { CssBaseline } from '@mui/material'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
+
+const theme = createTheme({
+	typography: {
+		fontFamily: [
+			'Outfit',
+			'Roboto',
+			'"Helvetica Neue"',
+			'Arial',
+			'sans-serif',
+		].join(','),
+	},
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<CssBaseline />
-			<App />
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<CssBaseline />
+				<App />
+			</BrowserRouter>
+		</ThemeProvider>
 	</React.StrictMode>
 )
